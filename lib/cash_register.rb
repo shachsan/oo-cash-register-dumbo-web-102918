ca22ccd77a2b1fccd32acd10a03248ee2bf5f9d9
SANJAY K SAI <shachsan@hotmail.com>
2018-11-02 14:37:33 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,7 +3,7 @@ class CashRegister
   def initialize(discount = 0)
     @total = 0
     @discount = discount
-    @total = @total - (discount/100)
+
   end
 
   def add_item(title, price, quantity = 1)
