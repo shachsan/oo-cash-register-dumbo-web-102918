5497d7f0e48c85d006fc37f5324d87e3e1ad8d9c
SANJAY K SAI <shachsan@hotmail.com>
2018-11-02 15:34:42 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -24,7 +24,7 @@ class CashRegister
   end
 
   def items
-    @@all_items.map {|item_price_pair|item_price_pair.first*item_price_pair}
+    @@all_items.map {|item_price_pair|item_price_pair.first*item_price_pair.last}
   end
 
   def void_last_transaction
