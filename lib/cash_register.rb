41a141c8958fcf013865d55950ed97848e373653
SANJAY K SAI <shachsan@hotmail.com>
2018-11-02 15:23:36 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -28,8 +28,8 @@ class CashRegister
   end
 
   def void_last_transaction
-    binding.pry
-    @@all_items.last.last - @@all_items.last.last 
+    # binding.pry
+    @@all_items.last.last - @@all_items.last.last
 
   end
 
