de1c4688fd6506f837b10bdef1783e7c9d08d72a
SANJAY K SAI <shachsan@hotmail.com>
2018-11-02 15:38:18 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -26,7 +26,7 @@ class CashRegister
   def items
     items_list = []
     @@all_items.each do |item_price_pair|
-      for each_item in 
+      for each_item in item_price_pair.last 
     end
   end
 
