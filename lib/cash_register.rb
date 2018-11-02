8d5defa73bb72508f3de4c441068dff1a4ea3978
SANJAY K SAI <shachsan@hotmail.com>
2018-11-02 14:25:03 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,9 +1,9 @@
 class CashRegister
-  attr_accessor :discount
+  attr_accessor :discount, :
   def initialize(discount = 0)
     @total = 0
     @discount = discount
     @total = @total - (discount/100)
   end
-  
+
 end
