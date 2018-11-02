0ecdf78f18b76a005f56ad0fe8172b88079e0613
SANJAY K SAI <shachsan@hotmail.com>
2018-11-02 15:08:09 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class CashRegister
   attr_accessor :discount, :total
-  @@all_items = []
+  # @@all_items = []
   def initialize(discount = 0)
     @total = 0
     @discount = discount
