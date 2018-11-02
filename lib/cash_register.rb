b0f09e43d77b20c4ec7f29f514a6940bc6b19a53
SANJAY K SAI <shachsan@hotmail.com>
2018-11-02 15:33:50 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
   end
 
   def add_item(title, price, quantity = 1)
-    @@all_items << [title, price,]
+    @@all_items << [title, price, quantity]
     @total += price * quantity
   end
 
